1cc207ffc67e43d3b0f10b7c9d67afddc6bb223f
Nick Rook <rooknj@outlook.com>
2019-03-03 13:19:39 -0500

    added comments and adjusted for readability

--- revuc/revuc.ino
+++ revuc/revuc.ino
@@ -5,13 +5,12 @@
 //*******************************************************************************
 #define NUM_LEDS 60                  // Number of LEDS in your strip
 #define NUM_LEDS_HALF (NUM_LEDS / 2) // Half the number of LEDS in your strip
-
-#define DATA_PIN 5 // Pin 5 on ESP8266
-#define buttonPin 6
-#define ANALOG_READ A5     //The pin that we read sensor values form
+#define DATA_PIN 5         // The data pin for the LED strip (green wire)
+#define buttonPin 6        // The pin hooked up to the push button
+#define ANALOG_READ A5     // The pin used to read the output signal from the microphone
 #define DC_OFFSET 0        // DC offset in mic signal - if unusure, leave 0
-#define NOISE 10           // Noise/hum/interference in mic signal
-#define TOP (NUM_LEDS + 2) // Allow dot to go slightly off scale
+#define NOISE 10           // Expected level of Noise/hum/interference in mic signal
+#define TOP (NUM_LEDS + 2) // The top of the led strip (used in height calculations) (is slightly off of the actual led strip)
 #define SAMPLES 60         // Length of buffer for dynamic level adjustment
 #define PEAK_FALL 40       // Rate of peak falling dot
 #define SPEED .20          // Amount to increment RGB color by each cycle
@@ -21,23 +20,24 @@ byte
     peak = 0,   // Used for falling dot
     height = 0; // Used for the body of the animation (current sound level)
 float
-    greenOffset = 30,
-    blueOffset = 150;
+    greenOffset = 30, // Starting green value for gradient changing animations
+    blueOffset = 150; // Starting blue value for gradient changing animations
 
 CRGB leds[NUM_LEDS]; // LED array
-int mode = 1;
+int mode = 1; // Current animation mode
 
 //*******************************************************************************
 // Setup
 //*******************************************************************************
 void setup()
 {
+  //Serial.begin(115200);
+  // Add leds and set brightness
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
-  setRGB(255, 0, 0);
-  Serial.begin(115200);
-  pinMode(buttonPin, INPUT);
-  analogReference(EXTERNAL);
+  setRGB(0, 0, 0);   // Clear the strip
+  pinMode(buttonPin, INPUT); // Set up the button pin to receive input
+  analogReference(EXTERNAL); // Remove microphone signal interferece from power supply
 }
 
 //*******************************************************************************
@@ -50,22 +50,22 @@ void loop()
   case 0:
     break;
   case 1:
-    vu();
+    dance1();
     break;
   case 2:
-    vu2();
+    dance2();
     break;
   case 3:
-    vu3();
+    dance3();
     break;
   case 4:
-    vu4();
+    dance4();
     break;
   case 5:
-    vu5();
+    dance5();
     break;
   case 6:
-    vu6();
+    dance6();
     break;
   }
   handleButton();
@@ -74,7 +74,7 @@ void loop()
 //*******************************************************************************
 // Helper Functions
 //*******************************************************************************
-void vu()
+void dance1()
 {
   calculatePeakAndHeight(TOP);
   // Color pixels based on rainbow gradient
@@ -83,15 +83,15 @@ void vu()
     if (i >= height)
       leds[i] = CRGB(0, 0, 0);
     else
-      leds[i] = getColorFromWheel(map(i, 0, NUM_LEDS - 1, 30, 150));
+      leds[i] = getColorFromGradient(map(i, 0, NUM_LEDS - 1, 30, 150));
   }
   // Draw peak dot
   if (peak > 0 && peak <= NUM_LEDS - 1)
-    leds[peak] = getColorFromWheel(map(peak, 0, NUM_LEDS - 1, 30, 150));
+    leds[peak] = getColorFromGradient(map(peak, 0, NUM_LEDS - 1, 30, 150));
   FastLED.show(); // Update strip
 }
 
-void vu2()
+void dance2()
 {
   calculatePeakAndHeight(TOP);
 
@@ -111,18 +111,18 @@ void vu2()
     }
     else
     {
-      leds[i] = getColorFromWheel(
+      leds[i] = getColorFromGradient(
           map(i, 0, NUM_LEDS - 1, (int)greenOffset, (int)blueOffset));
     }
   }
   // Draw peak dot
   if (peak > 0 && peak <= NUM_LEDS - 1)
-    leds[peak] = getColorFromWheel(map(peak, 0, NUM_LEDS - 1, 30, 150));
+    leds[peak] = getColorFromGradient(map(peak, 0, NUM_LEDS - 1, 30, 150));
 
   FastLED.show(); // Update strip
 }
 
-void vu3()
+void dance3()
 {
   calculatePeakAndHeight(TOP / 2);
   for (int i = 0; i < NUM_LEDS_HALF; i++)
@@ -134,7 +134,7 @@ void vu3()
     }
     else
     {
-      CRGB color = getColorFromWheel(map(i, 0, NUM_LEDS_HALF - 1, 30, 150));
+      CRGB color = getColorFromGradient(map(i, 0, NUM_LEDS_HALF - 1, 30, 150));
       leds[NUM_LEDS_HALF - i - 1] = color;
       leds[NUM_LEDS_HALF + i] = color;
     }
@@ -143,7 +143,7 @@ void vu3()
   // Draw peak dot
   if (peak > 0 && peak <= NUM_LEDS_HALF - 1)
   {
-    CRGB color = getColorFromWheel(map(peak, 0, NUM_LEDS_HALF - 1, 30, 150));
+    CRGB color = getColorFromGradient(map(peak, 0, NUM_LEDS_HALF - 1, 30, 150));
     leds[NUM_LEDS_HALF - peak - 1] = color;
     leds[NUM_LEDS_HALF + peak] = color;
   }
@@ -151,7 +151,7 @@ void vu3()
   FastLED.show(); // Update strip
 }
 
-void vu4()
+void dance4()
 {
   calculatePeakAndHeight(TOP / 2);
 
@@ -171,7 +171,7 @@ void vu4()
     }
     else
     {
-      CRGB color = getColorFromWheel(map(i, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
+      CRGB color = getColorFromGradient(map(i, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
       leds[NUM_LEDS_HALF - i - 1] = color;
       leds[NUM_LEDS_HALF + i] = color;
     }
@@ -180,7 +180,7 @@ void vu4()
   // Draw peak dot
   if (peak > 0 && peak <= NUM_LEDS_HALF - 1)
   {
-    CRGB color = getColorFromWheel(map(peak, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
+    CRGB color = getColorFromGradient(map(peak, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
     leds[NUM_LEDS_HALF - peak - 1] = color;
     leds[NUM_LEDS_HALF + peak] = color;
   }
@@ -188,7 +188,7 @@ void vu4()
   FastLED.show(); // Update strip
 }
 
-void vu5()
+void dance5()
 {
   calculatePeakAndHeight(TOP / 2);
   for (int i = 0; i < NUM_LEDS_HALF; i++)
@@ -200,7 +200,7 @@ void vu5()
     }
     else
     {
-      CRGB color = getColorFromWheel(map(i, 0, NUM_LEDS_HALF - 1, 30, 150));
+      CRGB color = getColorFromGradient(map(i, 0, NUM_LEDS_HALF - 1, 30, 150));
       leds[NUM_LEDS - i - 1] = color;
       leds[i] = color;
     }
@@ -209,7 +209,7 @@ void vu5()
   // Draw peak dot
   if (peak > 0 && peak <= NUM_LEDS_HALF - 1)
   {
-    CRGB color = getColorFromWheel(map(peak, 0, NUM_LEDS_HALF - 1, 30, 150));
+    CRGB color = getColorFromGradient(map(peak, 0, NUM_LEDS_HALF - 1, 30, 150));
     leds[NUM_LEDS - peak - 1] = color;
     leds[peak] = color;
   }
@@ -217,7 +217,7 @@ void vu5()
   FastLED.show(); // Update strip
 }
 
-void vu6()
+void dance6()
 {
   calculatePeakAndHeight(TOP / 2);
 
@@ -237,7 +237,7 @@ void vu6()
     }
     else
     {
-      CRGB color = getColorFromWheel(map(i, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
+      CRGB color = getColorFromGradient(map(i, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
       leds[NUM_LEDS - i - 1] = color;
       leds[i] = color;
     }
@@ -246,7 +246,7 @@ void vu6()
   // Draw peak dot
   if (peak > 0 && peak <= NUM_LEDS_HALF - 1)
   {
-    CRGB color = getColorFromWheel(map(peak, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
+    CRGB color = getColorFromGradient(map(peak, 0, NUM_LEDS_HALF - 1, (int)greenOffset, (int)blueOffset));
     leds[NUM_LEDS - peak - 1] = color;
     leds[peak] = color;
   }
@@ -261,27 +261,27 @@ void setRGB(uint8_t p_red, uint8_t p_green, uint8_t p_blue)
   FastLED.show();
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-CRGB getColorFromWheel(byte WheelPos)
+// Returns a gradient from blends between red-green-blue (input: 0-255)
+CRGB getColorFromGradient(byte gradientPosition)
 {
 
-  if (WheelPos < 85)
+  if (gradientPosition < 85)
   {
-    return CRGB((WheelPos * 3), (255 - WheelPos * 3), 0);
+    return CRGB((gradientPosition * 3), (255 - gradientPosition * 3), 0);
   }
-  else if (WheelPos < 170)
+  else if (gradientPosition < 170)
   {
-    WheelPos -= 85;
-    return CRGB((255 - WheelPos * 3), 0, (WheelPos * 3));
+    gradientPosition -= 85;
+    return CRGB((255 - gradientPosition * 3), 0, (gradientPosition * 3));
   }
   else
   {
-    WheelPos -= 170;
-    return CRGB(0, (WheelPos * 3), (255 - WheelPos * 3));
+    gradientPosition -= 170;
+    return CRGB(0, (gradientPosition * 3), (255 - gradientPosition * 3));
   }
 }
 
+// Detects when the button is pressed and switches between modes
 int reading;            // the current reading from the input pin
 int previous = LOW;     // the previous reading from the input pin
 long debounceStart = 0; // the last time the output pin was toggled
@@ -289,12 +289,11 @@ long debounce = 200;    // the debounce time, increase if the output flickers
 void handleButton()
 {
   reading = digitalRead(buttonPin);
-  // if the input just went from LOW and HIGH and we've waited long enough
-  // to ignore any noise on the circuit, toggle the output pin and remember
-  // the time
+  // Check if the input went from low to high, then debounce to prevent the button from accidently being clicked twice from bounce
   if (reading == HIGH && previous == LOW && millis() - debounceStart > debounce)
   {
     mode++;
+    // If you go past the last mode, reset to 0 (the off state) and clear the strip
     if (mode > 6)
     {
       mode = 0;
@@ -307,7 +306,8 @@ void handleButton()
 }
 
 // **************************************************************************************
-// DO NOT EDIT BELOW THIS LINE: COMPLEX AUDIO PROCESSING CODE
+// DO NOT EDIT BELOW THIS LINE: COMPLEX AUDIO PROCESSING getColorFromGradient
+//  inspired by https://learn.adafruit.com/led-ampli-tie/overview
 // **************************************************************************************
 byte dotCount = 0; // Frame counter for delaying dot-falling speed
 byte volCount = 0; // Frame counter for storing past volume data
