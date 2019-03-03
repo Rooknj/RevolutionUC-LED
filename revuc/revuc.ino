75e3602cebbd2657a1b4737b7dbe13c01d89f782
Nick Rook <rooknj@outlook.com>
2019-03-02 19:29:18 -0500

    got code working on arduino nano

--- revuc/revuc.ino
+++ revuc/revuc.ino
@@ -1,4 +1,3 @@
-//#define FASTLED_INTERRUPT_RETRY_COUNT 0
 #include "FastLED.h"
 
 //*******************************************************************************
@@ -6,12 +5,8 @@
 //*******************************************************************************
 #define NUM_LEDS 20 // Number of LEDS in your strip
 #define DATA_PIN 5 // Pin 5 on ESP8266
-#define buttonPin 12     // Pin 6 on ESP8266
-#define ANALOG_READ 17 //The pin that we read sensor values form
-#define DELAY 1 //Arduino loop delay
-
-#define MIC_LOW 500
-#define MIC_HIGH 1024
+#define buttonPin 6
+#define ANALOG_READ 0 //The pin that we read sensor values form
 
 // Globals
 CRGB leds[NUM_LEDS]; // LED array
@@ -31,36 +26,10 @@ void setup() {
 //*******************************************************************************
 // Loop
 //*******************************************************************************
-const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
-unsigned int sample;
+
 void loop() 
 {
-   unsigned long startMillis= millis();  // Start of sample window
-   unsigned int peakToPeak = 0;   // peak-to-peak level
-
-   unsigned int signalMax = 0;
-   unsigned int signalMin = 1024;
-
-   // collect data for 50 mS
-   while (millis() - startMillis < sampleWindow)
-   {
-      sample = analogRead(17);
-      if (sample < 1024)  // toss out spurious readings
-      {
-         if (sample > signalMax)
-         {
-            signalMax = sample;  // save just the max levels
-         }
-         else if (sample < signalMin)
-         {
-            signalMin = sample;  // save just the min levels
-         }
-      }
-   }
-   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
-
-   Serial.println(peakToPeak);
+   Serial.println(getMicSample());
    handleToggle();
 }
 
@@ -125,3 +94,30 @@ void handleToggle() {
   previous = reading;
 }
 
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+unsigned int getMicSample() {
+   unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 756;
+
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(ANALOG_READ);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   return peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+}
